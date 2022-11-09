33bc2f6c9a07988b5706a287fc2f1f74f12ceef1
ARMRusan <51991476+ARMRusan@users.noreply.github.com>
2022-11-09 13:19:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<html>
+ <head>
+    <title>SLIATE Result Management System</title>
+ </head>
+    <body> 
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <?php include('sidebar_dashboard2.php'); ?>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    
+                        <div class="span12" id="content">
+                            <div class="row-fluid">
+                            
+                                <!-- block -->
+                                <div class="block">
+                                    <div class="block-content collapse in">
+        <div id="sliderFrame">
+        <div id="slider">
+            <a href="http://www.sliate.ac.lk" target="_blank">
+                <img src="images/image-slider-1.jpg" alt="Welcome to www.sliate.ac.lk" />
+            </a>
+            <img src="images/image-slider-2.jpg" />
+            <img src="images/image-slider-3.jpg" alt="" />
+            <img src="images/image-slider-4.jpg" alt="#htmlcaption" />
+            <img src="images/image-slider-5.jpg" />
+               <img src="images/image-slider-6.jpg" />
+                  <img src="images/image-slider-7.jpg" />
+        </div>
+        <div id="htmlcaption" style="display: none;">
+        <a href="http://www.sliate.ac.lk/">SriLanaka Institute of Advanced Technological Education</a>
+        </div>
+    </div>
+                                                                  
+                                    </div>
+                                </div>
+                                <!-- /block -->
+                            </div>
+                        </div>
+<br>
+<br>
+<br>
+
+
+        <div class="span12" id="content">
+                            <div class="row-fluid">
+                              <div class="block">
+
+<p> <font color="black" size="4"> <b> (SLIATE) is one of the leading government institutions in higher education sector in Sri Lanka which has been established by the parliament act no 29 of 1995, and coming under the purview of the Ministry of Higher Education and Highways.
+
+There are 19 Advanced Technological Institutes operated by SLIATE island wide having conducted a broad range of multi-disciplinary programs targeting at A/L qualified students in Sri Lanka. </b> </font></p> 
+                
+                </div>
+                            </div>
+        </div>
+
+
+
+
+
+      <div class="span12" id="content">
+                            <div class="row-fluid">
+                              <div class="block">
+
+<p> <font color="black" size="4"> <b> 
+<br>
+<center>  Our Vision </center> 
+</b>
+<br>
+<center> <i> "To Become the Centre of Excellence in Technological Education"  </center> 
+<br>
+<b>
+<center>  Our Mission   </i> </center>
+<br>
+ </b>
+          <center>   <i>  "Creating Excellent Higher National and National Diplomates with Modern Technology for Sustainable Development"   </i>  </center>
+
+<br>
+</font>
+
+
+</p>
+
+<p>
+
+As per the recommendations of the Committee appointed by Prof. Wiswa Waranapala, Deputy Minister of Higher Education in 1994, the Sri Lanka Institute of Advanced Technical Education (SLIATE) was formed in 1995, under the Sri Lanka Institute of Advanced Technical Education Act No. 29 of 1995, In 2001 the name of the institution was amended to read as Sri Lanka Institute of Advanced Technological Education, (SLIATE).  It functions as an autonomous Institute for the management of Higher National and National Diploma courses. The main purposes of establishing SLIATE were to reform and restructure the entire technical and vocational education system in relation to the changing needs of economic development, to meet manpower requirements of national development strategies, and the promotion of privatization, with special concern for meeting the scarcity of trained technological manpower resources at the technician level.
+
+The SLIATE is a statutory body operating under the purview of Ministry of Higher Education and is one of the leading higher educational institutions in Srilanka.
+
+SLIATE has been focusing on fostering Advanced Technical Education at a post secondary level. It is mandated to establish Advanced Technological Institute (ATI) in every province for technological courses. At present it manages and supervises Twelve  Advanced Technological Institutes and Six Sections. SLIATE is headed by a Director General while each of its institutes is headed by a Director. All academic sections of SLIATE and its institutions are headed by Academic Coordinators.  </p> 
+                
+                </div>
+                            </div>
+        </div>
+
+
+
+
+
+
+
+            </div>
+     
+        </div>
+
+        <?php include('footer.php'); ?>
+      
+    </body>
+  <link href="themes/1/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/1/js-image-slider.js" type="text/javascript"></script>
+    <link href="generic.css" rel="stylesheet" type="text/css" />
+</html>
